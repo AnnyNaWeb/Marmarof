e89d5f31be9b39e5d539447817c906389389ca86
annynaweb <sousa.annyc@gmail.com>
2020-08-01 00:43:16 -0300

    Scene Manager

---
+++ Assets/Script/Cena.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cena : MonoBehaviour
+{
+
+   public void Jogar()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+     public void Opcoes()
+    {
+
+        SceneManager.LoadScene("Options");
+    }
+     public void Voltar()
+    {
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Sair(){
+        Application.Quit();
+    }
+
+
+}
