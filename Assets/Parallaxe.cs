2eba917aceaea25e037826e4d048d322ee44bbe2
annynaweb <sousa.annyc@gmail.com>
2020-08-02 02:37:05 -0300

    PARALLAXE MALDITO

---
+++ Assets/Parallaxe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxe : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float speedText;
+
+    void Update(){
+        float offset = Time.time * speedText;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+    }
+}
